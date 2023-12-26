1740d67ee2cc6ce9f7059f4b31aa29fe73fb0505
saritalad <sarita.lad@rediffmail.com>
2023-12-26 16:00:43 +0530

    Add project files.

---
+++ webapi_azureSQL23/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi_azureSQL23.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi_azureSQL23.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context=context;
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+
+       
+        
+    }
+}
---
+++ webapi_azureSQL23/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi_azureSQL23.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public Decimal? Price { get; set; }
+        public DateTime mfd { get; set; }
+    }
+}
